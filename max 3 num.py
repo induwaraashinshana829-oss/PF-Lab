5ad1ddf0ab90f9d7d2e02d904a974620932bbaba
Ashinshana Edirisinghe <induwaraashinshana829@gmail.com>
2026-05-31 05:19:56 +0530

    Add files via upload

---
+++ max 3 num.py
@@ -0,0 +1,26 @@
+
+
+
+x = int(input("Enter the num01"))
+y = int(input("Enter the num02"))
+z = int(input("Enter the num03"))
+
+if x>y:
+    if:
+      x>z:
+        print("Max is ",x)
+    else:
+        print("Max is ",Z)
+
+else:
+    if y>z:
+        print("Max is ",y)
+    else
+        print("Max is ",z)
+    
+    
+        
+
+
+
+                 
