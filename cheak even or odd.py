cebacd7fa8b12b5da1eb7375008bfd6efc330c60
Ashinshana Edirisinghe <induwaraashinshana829@gmail.com>
2026-05-31 05:19:25 +0530

    Add files via upload

---
+++ cheak even or odd.py
@@ -0,0 +1,9 @@
+
+
+num=int(input("Enter the Number"))
+
+if num % 2 == 0 :
+    print("This Number Is Even",num)
+
+else :
+    print("This Number Is Odd",num)
---
+++ cheak person eligibility.py
@@ -0,0 +1,10 @@
+
+#insert age,
+age=int(input("Enter your age"))
+
+#decision making,
+if age>=18 :
+    print("Eligible to Vote!")
+
+else:
+    print("Not Eligible to Vote!")
